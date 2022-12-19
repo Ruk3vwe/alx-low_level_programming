647ac220dea7ead963e0b40891e19996c553ddc3
Ruk3vwe <anitod@live.co.uk>
2022-12-19 00:45:13 -0800

    debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,6 +1,6 @@
 
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef main_h
+#define main_h
 
 #include <stdio.h>
 
@@ -9,4 +9,4 @@ int largest_number(int, int, int);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 
-#endif /* MAIN_H */
+#endif /* main_h */

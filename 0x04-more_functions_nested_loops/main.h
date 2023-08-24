55779f536def4a32b06656733209178ef10c46be
Ruk3vwe <anitod@live.co.uk>
2023-08-24 11:17:12 -0700

    more

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -10,3 +10,11 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+void print_integer(int m);
+int _putchar(char c);
+
+#endif /* MAIN_H */

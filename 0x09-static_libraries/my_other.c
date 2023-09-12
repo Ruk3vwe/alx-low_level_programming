aa81af519c3abeb53fc3ca8eca8553f767820924
Ruk3vwe <anitod@live.co.uk>
2023-09-11 21:27:35 -0700

    static

---
+++ 0x09-static_libraries/my_other.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+int _strcmp(char *s1, char *s2)
+{
+    // Implementation for _strcmp
+}
+
+char *_memset(char *s, char b, unsigned int n)
+{
+    // Implementation for _memset
+}
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+    // Implementation for _memcpy
+}
+
+char *_strchr(char *s, char c)
+{
+    // Implementation for _strchr
+}
+
+unsigned int _strspn(char *s, char *accept)
+{
+    // Implementation for _strspn
+}
+
+char *_strpbrk(char *s, char *accept)
+{
+    // Implementation for _strpbrk
+}
+
+char *_strstr(char *haystack, char *needle)
+{
+    // Implementation for _strstr
+}
